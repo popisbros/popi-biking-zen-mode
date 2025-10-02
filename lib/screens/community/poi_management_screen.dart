25f8abf0811a5939a8b2dd1ef6d0c1fd37806465
Sylvain Brosset <sylvain@popisair.home>
2025-10-02 18:06:31 +0200

    fix: Correct AppColors import in community screens

--- lib/screens/community/poi_management_screen.dart
+++ lib/screens/community/poi_management_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../constants/app_theme.dart';
+import '../../constants/app_colors.dart';
 import '../../providers/community_provider.dart';
 import '../../providers/location_provider.dart';
 import '../../models/cycling_poi.dart';
